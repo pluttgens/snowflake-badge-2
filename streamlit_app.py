db2a6ec8f529f168b1f17fae6d0ce1f9fbb9d8b2
Pascal Luttgens <pascal.luttgens+git@proton.me>
2023-09-25 13:57:55 +0200

    add requests and fruit advice section

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import requests
 import pandas as pd
 
 streamlit.title("My Parents New Healthy Diner")
@@ -22,3 +23,8 @@ fruits_selected = streamlit.multiselect(
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header("Fruityvice Fruit Advice!")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
